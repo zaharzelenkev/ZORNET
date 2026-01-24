09acd7a0a6fd8d37bedc7e4c23c3dfe47d7a4e14
Захар Зеленкевич <zahar.zelenkevv@gmail.com>
2026-01-24 23:46:22 +0300

    Update zornet.py

--- zornet.py
+++ zornet.py
@@ -604,9 +604,9 @@ if st.session_state.page == "Главная":
     
         <div class="search-container">
             <form action="https://www.google.com/search" method="get" target="_top">
-                <input type="text" name="q" placeholder="🔍 Введите запрос..." required autocomplete="off">
+                <input type="text" name="q" placeholder="🔍 Введите запрос" required autocomplete="off">
                 <br>
-                <button type="submit">ИСКАТЬ</button>
+                <button type="submit">Поиск</button>
             </form>
         </div>
 
