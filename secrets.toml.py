cb6bda4bdf0a8a65a5b082536d81b849d4eadf40
Захар Зеленкевич <zahar.zelenkevv@gmail.com>
2026-01-31 17:17:23 +0300

    Update secrets.toml.py

--- secrets.toml.py
+++ secrets.toml.py
@@ -1,1 +1,4 @@
-HF_API_KEY = "h_XzFyShNnTByfEsHPIehaA£hMtECtGWLjMk"  # <--- ВСТАВЬ СВОЙ НОВЫЙ ТОКЕН
+# .streamlit/secrets.toml
+GOOGLE_CLIENT_ID = "554559620450-u8kilhp2pgk7k2juarujah3c5pea3g94.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_REDIRECT_URI = "https://zornet.streamlit.app"
